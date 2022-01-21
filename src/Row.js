1efa1490a7825ffdce33ba8f3441ee2841bce5f1
unknown <saounmustafa@gmail.com>
2022-01-22 03:26:17 +0500

    trailer-popups added

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,13 @@
 import axios from "./axios";
 import React from "react";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
 const baseImg_url = "https://image.tmdb.org/t/p/original/";
 const Row = (props) => {
   const [movies, setMovies] = React.useState([]);
-
+  const [trailerUrl, setTrailerUrl] = React.useState("");
   React.useEffect(() => {
     getData();
   }, [props.fetchUrl]);
@@ -14,6 +17,27 @@ const Row = (props) => {
     setMovies(request.data.results);
     //  console.log(movies);
   };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log("THIS IS THE ", url);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2 className="row_title">{props.title}</h2>
@@ -21,6 +45,7 @@ const Row = (props) => {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            onClick={() => handleClick(movie)}
             key={movie.id}
             className={`poster ${props.isLargeRow && "large_poster"}`}
             src={`${baseImg_url}${
@@ -30,6 +55,7 @@ const Row = (props) => {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
